2980b19ff7f84e6c50b512af89778e4953c0cb7b
Tamir <tmelzerb@gmail.com>
2025-07-27 10:57:32 -0700

    fix: simpler lsp for ruby

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,23 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "solargraph" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
-local lspconfig = require "lspconfig"
-
-lspconfig.solargraph.setup {
-  cmd = { "solargraph", "stdio" },
-  filetypes = { "ruby" },
-  root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-  settings = {
-    solargraph = {
-      diagnostics = true,
-      completion = true,
-      hover = true,
-      autoformat = false,
-      formatting = false,
-    }
-  }
-}
-
