e3cd5dda61eaffcb42b677ff81c2775e4fb1e013
Tamir <tmelzerb@gmail.com>
2025-07-27 10:53:33 -0700

    feat: solargraph lsp support

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,3 +4,20 @@ local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+local lspconfig = require "lspconfig"
+
+lspconfig.solargraph.setup {
+  cmd = { "solargraph", "stdio" },
+  filetypes = { "ruby" },
+  root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+  settings = {
+    solargraph = {
+      diagnostics = true,
+      completion = true,
+      hover = true,
+      autoformat = false,
+      formatting = false,
+    }
+  }
+}
+
