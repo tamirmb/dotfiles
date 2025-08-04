ff1e1be895ba25311a3f62bea2d23857162c1dee
Tamir <tmelzerb@gmail.com>
2025-08-03 19:01:09 -0700

    feat: relative line numbers and more lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "solargraph" }
+local servers = { "html", "cssls", "solargraph", "ts_ls", "tailwindcss" }
+
+vim.lsp.config.ts_ls = {
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+}
+
+vim.lsp.config.html = {}
+vim.lsp.config.cssls = {}
+vim.lsp.config.solargraph = {}
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
